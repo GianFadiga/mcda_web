ab5c9df62accd6528bcc4e26d59cb4f23c54ae35
GianFadiga <91998287+GianFadiga@users.noreply.github.com>
2025-06-06 10:35:32 -0300

    Update settings.py

--- mcda_project/mcda_project/settings.py
+++ mcda_project/mcda_project/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'analyzer_app',
+    'mcda_project.analyzer_app',
     'widget_tweaks',
 ]
 
