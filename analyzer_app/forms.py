4aff8ce7938acb003fcdb0cad0ed0d219f863601
GianFadiga <91998287+GianFadiga@users.noreply.github.com>
2025-08-11 17:42:35 -0300

    Update forms.py

--- analyzer_app/forms.py
+++ analyzer_app/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 User = get_user_model()
 
